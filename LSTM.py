1e907a200ef1726a5935c02a0c181be01288056c
AlbinWeinz <118757674+AlbinWeinz@users.noreply.github.com>
2025-03-28 17:22:06 +0100

    function for team_average_rating, cleanup

--- LSTM.py
+++ LSTM.py
@@ -24,7 +24,7 @@ team_attr_df = pd.read_sql_query("SELECT * FROM Team_Attributes", conn)
 match_df = match_df.sort_values(by="date")
 
 # match_df = match_df.dropna(how="any", axis=0) # DROPS ALL ROWS WITH MISSING VALUES, THIS REDUCES SIZE FROM 25979 ROWS TO 1762, need better cleaning
-# player_attr_df = player_attr_df.dropna(how="any", axis=0)
+player_attr_df = player_attr_df.dropna(how="any", axis=0)
 match_df["date"] = pd.to_datetime(match_df["date"])
 player_attr_df["date"] = pd.to_datetime(player_attr_df["date"])
 
@@ -32,35 +32,11 @@ team_games = {team: [] for team in team_df["team_api_id"]}
 x = []
 y = []
 
-
-#fails for null values
-def get_player_attributes_closest_to_match_date(player_api_id, match_date):
-    player_rating_history = player_attr_df[player_attr_df["player_api_id"] == player_api_id]
-
-    player_rating_history = player_rating_history.set_index("date").sort_index()
-
-    iloc_idx = player_rating_history.index.get_indexer([match_date], method="nearest")
-    loc_idx = player_rating_history.index[iloc_idx]
-    closest_row = player_rating_history.loc[loc_idx]
-    return closest_row.iloc[0]
-
 for _, row in match_df.iterrows():
     home_players_stats = []
     away_players_stats = []
     match_date = row["date"]
-    home_team_average_rating = 0
-    away_team_average_rating = 0
-    for i in range(11):
-        home_player_api = row[f"home_player_{i+1}"]
-        away_player_api = row[f"away_player_{i+1}"]
-        home_player_attr_for_match = get_player_attributes_closest_to_match_date(home_player_api, match_date)
-        away_player_attr_for_match = get_player_attributes_closest_to_match_date(away_player_api, match_date)
-        home_team_average_rating += home_player_attr_for_match["overall_rating"]
-        away_team_average_rating += away_player_attr_for_match["overall_rating"]
 
-    home_team_average_rating = home_team_average_rating / 11
-    away_team_average_rating = away_team_average_rating / 11
-    rating_diff = home_team_average_rating - away_team_average_rating
     home_team = row["home_team_api_id"]
     away_team = row["away_team_api_id"]
     home_team_goal = row["home_team_goal"]
@@ -92,8 +68,8 @@ for _, row in match_df.iterrows():
 
 
 
-            features.append([h[0]-a[0], h[1]-a[1]])
-            # features.append([h-a])
+            # features.append([h[0]-a[0], h[1]-a[1]])
+            features.append([h-a])
 
 
         
@@ -101,10 +77,10 @@ for _, row in match_df.iterrows():
         y.append(outcome)  
 
 
-    team_games[home_team].append([goal_diff, rating_diff])
-    team_games[away_team].append([goal_diff, rating_diff]) 
-    # team_games[home_team].append(goal_diff)
-    # team_games[away_team].append(goal_diff) 
+    # team_games[home_team].append([goal_diff, rating_diff])
+    # team_games[away_team].append([goal_diff, rating_diff]) 
+    team_games[home_team].append(goal_diff)
+    team_games[away_team].append(goal_diff) 
 
 
 # print(x)
--- data_exploration.py
+++ data_exploration.py
@@ -17,22 +17,13 @@ match_df = pd.read_sql_query("SELECT * FROM Match", conn)
 player_df = pd.read_sql_query("SELECT * FROM Player", conn)
 player_attr_df = pd.read_sql_query("SELECT * FROM Player_Attributes", conn)
 team_attr_df = pd.read_sql_query("SELECT * FROM Team_Attributes", conn)
-match_df = match_df.sort_values(by="date")
+match_df = match_df.sort_values(by="date") #dunno if this is needed
 
 
-# print(match_df["shoton"].isnull().sum())
-# print(match_df["shotoff"].isnull().sum())
-# print(match_df["foulcommit"].isnull().sum())
-# print(match_df["cross"].isnull().sum())
-# print(match_df["corner"].isnull().sum())
-# print(match_df["possession"].isnull().sum())
-# print(match_df[["card", "shoton"]].isnull().sum())
-# print(match_df.shape[0])
-# match_df = match_df.dropna(how="any", axis=0)
+match_df["date"] = pd.to_datetime(match_df["date"])
+player_attr_df["date"] = pd.to_datetime(player_attr_df["date"])
 
-# print(match_df.shape[0])
-
-# print(match_df.isnull().sum())
+print(match_df)
 
 home_player_cols = ["home_player_1", "home_player_2", "home_player_3", "home_player_4", "home_player_5",
                "home_player_6", "home_player_7", "home_player_8", "home_player_9", "home_player_10",
@@ -53,7 +44,6 @@ match_df["betting_odds_home_avrg"] = betting_odds_home.mean(axis=1)
 match_df["betting_odds_draw_avrg"] = betting_oods_draw.mean(axis=1)
 match_df["betting_odds_away_avrg"] = betting_oods_away.mean(axis=1)
 
-print(match_df)
 
 match_df = match_df.drop(match_df.columns[41:71], axis=1)
 
@@ -65,53 +55,45 @@ match_df = match_df.dropna(subset=away_player_cols)
 
 match_df = match_df.dropna(how="any", axis=0)
 
-print(match_df.isna().sum())
-print(match_df.shape[0]) #This creates a dataset with 13221 matches, with 0 missing values
-
-# print(match_df["betting_avg_home"].notnull().sum())
-
-# print(match_df["betting_avg_home"].isnull().sum())
-
-# betting_df = match_df[betting_cols]
-
-# print(betting_df.shape[0])
-# # betting_df = betting_df.dropna(how="all", axis=0)
-# print(betting_df.shape[0])
-# print(betting_df)
-# print(betting_df)
-# match_df = match_df[[
-#     "home_player_1","home_player_2","home_player_3","home_player_4","home_player_5","home_player_6","home_player_7","home_player_8","home_player_9","home_player_10","home_player_11",
-#     "away_player_1", "away_player_2","away_player_3","away_player_4","away_player_5","away_player_6","away_player_7","away_player_8","away_player_9","away_player_10","away_player_11"]].dropna(how="any", axis=0)
-
-# print(match_df[[
-#     "home_player_1","home_player_2","home_player_3","home_player_4","home_player_5","home_player_6","home_player_7","home_player_8","home_player_9","home_player_10","home_player_11",
-#     "away_player_1", "away_player_2","away_player_3","away_player_4","away_player_5","away_player_6","away_player_7","away_player_8","away_player_9","away_player_10","away_player_11"]].isnull().sum())
-# print(match_df.shape)
-
-# print(match_df.isnull().sum())
-
-# team_games = {team: [] for team in match_df["home_team_api_id"].unique()}
-
-# team_games2 = {team: [] for team in team_df["team_api_id"]}
-
-# print(len(team_games))
-# print(len(team_games2))
+player_attr_df = player_attr_df.dropna(subset=["date", "overall_rating"])
 
 
+#function for getting a players attribute nearest in time to the match_date
+def get_player_attributes_closest_to_match_date(player_api_id, match_date): 
+    player_rating_history = player_attr_df[player_attr_df["player_api_id"] == player_api_id]
 
-# print(player_attr_df.isnull().sum())
-# print(player_attr_df.size)
-# player_attr_df = player_attr_df.dropna(how="any", axis=0)
-# print(player_attr_df.isnull().sum())
-# print(player_attr_df)
+    player_rating_history = player_rating_history.set_index("date").sort_index()
 
-# print(match_df["home_player_1"].isnull().sum())
+    iloc_idx = player_rating_history.index.get_indexer([match_date], method="nearest")
+    loc_idx = player_rating_history.index[iloc_idx]
+    closest_row = player_rating_history.loc[loc_idx]
+    return closest_row.iloc[0]
+
+match_df["home_team_rating_avrg"] = None
+match_df["away_team_rating_avrg"] = None
+
+#function for computing the home and away team_rating_avrgs of a row in the match_data_df
+# Pretty slow process, takes roughly 6 min
+def compute_team_ratings(row):
+    match_date = row["date"]
+    home_team_rating_avrg = 0
+    away_team_rating_avrg = 0
+    
+    for i in range(11):
+        home_player_api = row[f"home_player_{i+1}"]
+        away_player_api = row[f"away_player_{i+1}"]
+
+        home_player_attr_for_match = get_player_attributes_closest_to_match_date(home_player_api, match_date)
+        away_player_attr_for_match = get_player_attributes_closest_to_match_date(away_player_api, match_date)
+
+        home_team_rating_avrg += home_player_attr_for_match["overall_rating"]
+        away_team_rating_avrg += away_player_attr_for_match["overall_rating"]
+    
+    row["home_team_rating_avrg"] = home_team_rating_avrg / 11
+    row["away_team_rating_avrg"] = away_team_rating_avrg / 11
+    return row
+
+# match_df = match_df.apply(compute_team_ratings, axis=1) //No point in running this unless we are creating the finalized set of features to be used
 
 
-# history.
-# print(history["date"])
-
-# iloc_idx = history.index.get_indexer([x], method="nearest")
-# val = history[iloc_idx]
-# print(val)
-# print(player_history)
+print(match_df)

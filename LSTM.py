c2beedf76fe7eb5462405fb42325f24b41fe1969
AlbinWeinz <118757674+AlbinWeinz@users.noreply.github.com>
2025-04-15 14:04:42 +0200

    changed search method, plotting + model results

--- LSTM.py
+++ LSTM.py
@@ -4,7 +4,8 @@ from pathlib import Path
 import keras
 from sklearn.preprocessing import MinMaxScaler
 import keras_tuner as kt
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
 
 
 current_dir = Path(__file__).parent
@@ -64,19 +65,6 @@ y_val = y[train_end:val_end]
 x_test = x[val_end:]
 y_test = y[val_end:]
 
-# num_draw = 0
-# num_home = 0
-# num_away = 0
-# for c in range(len(y_test)):
-#     if y_test[c] == 0:
-#         num_draw+=1
-#     elif y_test[c] == 1:
-#         num_home+=1
-#     else:
-#         num_away+=1
-
-# print(num_draw, num_home, num_away)
-
 x_train = x_train.reshape(x_train.shape[0], 5, 26)
 x_val = x_val.reshape(x_val.shape[0], 5, 26)
 x_test = x_test.reshape(x_test.shape[0], 5, 26)
@@ -84,14 +72,14 @@ y_train = keras.utils.to_categorical(y_train, num_classes=3)
 y_val = keras.utils.to_categorical(y_val, num_classes=3)
 y_test = keras.utils.to_categorical(y_test, num_classes=3)
 
+lr_values = np.logspace(-5, -1, num=50)
 def build_model(hp):
     model = keras.Sequential()
     model.add(keras.layers.LSTM(64))
     model.add(keras.layers.Dense(32, activation="relu"))
     model.add(keras.layers.Dense(3, activation="softmax"))
-    hp_learning_rate = hp.Float(
-            "learning_rate", min_value=1e-4,
-            max_value=1, sampling="log")
+    hp_learning_rate = hp.Choice("learning_rate",
+                                    values=lr_values.tolist())
     rmsprop = keras.optimizers.RMSprop(
         learning_rate=hp_learning_rate)
     model.compile(optimizer=rmsprop,
@@ -99,10 +87,30 @@ def build_model(hp):
                     metrics=["accuracy", "precision",
                               "recall", "f1_score"])
     return model
-tuner = kt.RandomSearch(build_model, objective="val_loss",
-                         overwrite=True, max_trials=5)
+tuner = kt.GridSearch(build_model, objective="val_loss",
+                         overwrite=True, max_trials=50)
 tuner.search(x_train, y_train,
-              validation_data=(x_val, y_val), epochs=10)
+              validation_data=(x_val, y_val), 
+              epochs=10, shuffle=False,)
+
+trials = tuner.oracle.get_best_trials(num_trials=50)
+learning_rates = []
+accuracies = []
+for trial in tuner.oracle.trials.values():
+    lr = trial.hyperparameters.get("learning_rate")
+    acc = trial.metrics.get_best_value("val_accuracy")
+    learning_rates.append(lr)
+    accuracies.append(acc)
+learning_rates = list(map(float, learning_rates))
+
+plt.figure(figsize=(8, 5))
+plt.plot(learning_rates, accuracies, marker="o")
+plt.xscale("log")  
+plt.title("Learning Rate effect on model Accuracy")
+plt.xlabel("Learning Rate")
+plt.ylabel("Validation Accuracy")
+plt.grid(True)
+plt.show()
 
 best_hp = tuner.get_best_hyperparameters(num_trials=1)[0]
 model = tuner.hypermodel.build(best_hp)
@@ -113,13 +121,26 @@ early_stopping = keras.callbacks.EarlyStopping(
 )
 model.fit(x_train, y_train, validation_data=(x_val, y_val),
            epochs=50, callbacks=early_stopping, shuffle=False)
-
-test_loss, test_accuracy, test_precision, test_recall, test_f1 = model.evaluate(x_test, y_test)
+t_loss, t_accuracy, t_precision, t_recall, t_f1 = model.evaluate(x_test, y_test)
 print("Best LR", best_hp["learning_rate"])
-print("Test accuracy:", test_accuracy)
-print("Test precision:", test_precision)
-print("Test recall:", test_recall)
-print("Test F1-score:", test_f1)
+print("Test accuracy:", t_accuracy)
+print("Test precision:", t_precision)
+print("Test recall:", t_recall)
+print("Test F1-score:", t_f1)
+
+y_prediction = model.predict(x_test)
+y_pred_labels = np.argmax(y_prediction, axis=1)
+y_true_labels = np.argmax(y_test, axis=1)
+
+class_names = ["draw", "home_win", "away_win"]
+
+print("\nPer-class classification report:")
+print(classification_report(y_true_labels, y_pred_labels, target_names=class_names, digits=4))
+
+c_matrix = confusion_matrix(y_true_labels, y_pred_labels, normalize="pred")
+print("\nConfusion matrix (normalized by prediction):")
+print("Labels: ", class_names)
+print(c_matrix)
 
 y_prediction = model.predict(x_test)
 y_prediction = np.argmax (y_prediction, axis = 1)
